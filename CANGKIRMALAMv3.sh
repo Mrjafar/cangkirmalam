938e232d9b467d46c90d1fbe1f2724dcbfc6fb66
Mrjafar <55581208+Mrjafar@users.noreply.github.com>
2019-09-21 14:01:20 +0700

    Add files via upload

---
+++ CANGKIRMALAMv3.sh
@@ -0,0 +1,95 @@
+#!/bin/sh
+
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+echo
+toilet -f big -F gay MR_J4F4R
+echo "------------------------------------------------------------------"
+echo $green"[+]SELAMAT DATANG[+]"
+echo "------------------------------------------------------------------"
+echo "__________________________________________"
+echo
+echo $yellow"[*]SILAHKAN PILIH TOOLSNYA[*] :"
+echo "__________________________________________"
+echo "##############################"|lolcat
+echo $cyan"[+]1.) STABILKAN JARINGAN"|lolcat
+echo "##############################"|lolcat
+echo $red"[+]2.) HACK FACEBOOK"|lolcat
+echo "##############################"|lolcat
+echo $blue"[+]3.) LACAK LOKASI"|lolcat
+echo "##############################"|lolcat
+echo $cyan"[+]4.) MASUK KE TOOLS"|lolcat
+echo "##############################"|lolcat
+echo $purple"[+]0.) GET OUT.!!!"|lolcat
+echo "##############################"|lolcat
+
+
+echo $green
+read -p "PILIH MANA : " bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+toilet -f big -F gay MR_J4F4R
+echo $green"[+]Stabilkan Jaringan By Mr_J4F4R"
+sleep 2
+ping -s1000 1.1.1.1
+fi
+
+
+echo $green
+echo "=================================================================="
+figlet "                 TOOLS  MR_J4F4R "
+echo "=================================================================="
+echo "SORRY GAN SERVER TOOLS LG EROR PILIH NO YANG LAIN OK"
+
+if [ $bro = 2 ] || [ $bro =2 ]
+then
+clear
+git clone https://github.com/sixtysix-Team/fbbrute
+cd fbbrute
+pip2 install mechanize
+python2 force.py
+fi
+
+echo $yellow
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+clear
+git clone https://github.com/thelinuxchoice/locator
+cd locator
+tsu
+bash locator.sh
+fi
+done
+
+
+echo $green
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+clear
+git clone https://github.com/rahmadxyz/ToolsLengakap.git
+cd ToolsLengkap
+sh rahmad.sh
+fi
+done
+
+
+if [ $bro = 0 ] || [ $bro = 0 ]
+then
+clear
+echo $yellow
+figlet "bye"
+echo $white"To Be Continued"
+sleep 3
+exit
+fi
+
+done
